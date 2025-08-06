1a4d7c7493a78c777f6f0658dc4a28964b910141
ljseo <wnstj7866@naver.com>
2025-08-07 06:32:02 +0900

    [Feat] gateway project 생성 및 기본 파일 추가

---
+++ src/main/java/com/ssafy/tooguigateway/TooguiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.tooguigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TooguiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TooguiGatewayApplication.class, args);
+    }
+
+}
